339854249e03c1a15227f8db726133813b9fbd9a
codingories <tellt@126.com>
2019-09-06 09:59:58 +0800

    简单购物车的实现

--- index.js
+++ index.js
@@ -6,31 +6,37 @@ var app = new Vue({
         id: 1,
         name: "iPhone 7",
         price: 6188,
-        count: 1
+        count: 1,
+        isBuy: false
       },
       {
         id: 2,
         name: "iPad Pro",
         price: 5888,
-        count: 1
+        count: 1,
+        isBuy: false
       },
       {
         id: 3,
         name: "MacBook Pro",
         price: 21488,
-        count: 1
+        count: 1,
+        isBuy: false
       }
     ],
-    checkBoxModel: []
+    checkBoxModel: [],
+    allp: false
   },
   computed: {
     totalPrice: function() {
       var total = 0;
       for (var i = 0; i < this.list.length; i++) {
-        var item = this.list[i];
-        total += item.price * item.count;
+        if (this.list[i].isBuy) {
+          var item = this.list[i];
+          total += item.price * item.count;
+        }
       }
-      return total.toString().replace(/\B(?=(\d{3})+$)/g, ",");
+      return total.toString().replace(/\B(?=(\d{3})+$)/g, "", "");
     }
   },
   methods: {
@@ -43,6 +49,63 @@ var app = new Vue({
     },
     handleRemove: function(index) {
       this.list.splice(index, 1);
+    },
+    allPick: function() {
+      var _this = this;
+      if (_this.allp) {
+        _this.checkBoxModel = [];
+        _this.allp = false;
+      } else {
+        _this.checkBoxModel = [];
+        _this.list.forEach(function(item) {
+          _this.checkBoxModel.push(item.id);
+        });
+        _this.allp = true;
+      }
+      //全选的实现通过checkBoxModel的状态
+    },
+    pickOne: function(index) {
+      if (this.list[index].isBuy) {
+        this.list[index].isBuy = false;
+      } else {
+        this.list[index].isBuy = true;
+      }
+      //单选的实现依靠的是isBuy通过click的切换实现
+    },
+    checkPick: function() {
+      _this = this;
+      var sumPic = 0;
+      for (var i = 0; i < _this.list.length; i++) {
+        if (_this.list[i].isBuy) {
+          sumPic++;
+        }
+      }
+      if (sumPic == _this.list.length) {
+        _this.allp = true;
+      } else {
+        _this.allp = false;
+      }
+    },
+    checkModel: function() {
+      _this = this;
+      if (_this.checkBoxModel.length) {
+        newArr = _this.checkBoxModel.concat();
+        console.log(newArr);
+        for (var i = 0; i < _this.checkBoxModel.length; i++) {
+          newone = newArr.shift().toString();
+          // console.log(newone);
+          _this.list[newone - 1].isBuy = true;
+          // console.log(newone);
+          // console.log(_this.list[newone - 1]);
+        }
+      } else {
+        newArr = _this.checkBoxModel.concat();
+        //console.log(newArr);
+        for (var i = 0; i < _this.list.length; i++) {
+          _this.list[i].isBuy = false;
+        }
+      }
+      // 利用checkBoxModel的绑定的状态来分别给每个物品确认isBuy的状态，避免与pickOne的冲突
     }
   }
 });
